597515df6c7dd7c1ee4d4e3c32b712cfdcdec991
gustavocastro <gustavoprasca@hotmail.com>
2023-03-03 06:51:47 -0500

    Se agrega otro archivo como fuente de datos SQL y se realizan pruebas unitarias para query Methods y @query agregados en el repositorio

--- src/main/java/com/co/softworld/dao/IProductDao.java
+++ src/main/java/com/co/softworld/dao/IProductDao.java
@@ -2,6 +2,13 @@ package com.co.softworld.dao;
 
 import com.co.softworld.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface IProductDao extends JpaRepository<Product, Integer> {
+    Optional<Product> findByName(String name);
+
+    @Query("select p from Product p where p.name=?1")
+    Optional<Product> findByNameQuery(String name);
 }
--- src/test/java/com/co/softworld/dao/ProductDaoTest.java
+++ src/test/java/com/co/softworld/dao/ProductDaoTest.java
@@ -8,9 +8,11 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -29,7 +31,28 @@ class ProductDaoTest {
         List<Product> products = new ArrayList<>();
         products.add(new Product(1, "computer", 400));
         products.add(new Product(2, "mouse", 10));
+        products.add(new Product(3, "desktop", 500));
         assertEquals(products, productDao.findAll());
         assertThat(productDao.findAll(), equalTo(products));
+    }
+
+    @Test
+    void testFindByName_null() {
+        assertThat(productDao.findByName("laptop").orElse(new Product()).getName(), nullValue());
+    }
+
+    @Test
+    void testFindByName_assertThrow() {
+        assertThrows(NoSuchElementException.class, productDao.findByName("laptop")::orElseThrow);
+    }
+
+    @Test
+    void testFindByName() {
+        assertThat(productDao.findByName("computer").orElse(new Product()).getName(), equalTo("computer"));
+    }
+
+    @Test
+    void testFindByNameQuery() {
+        assertThat(productDao.findByNameQuery("computer").orElse(new Product()).getName(), equalTo("computer"));
     }
 }
